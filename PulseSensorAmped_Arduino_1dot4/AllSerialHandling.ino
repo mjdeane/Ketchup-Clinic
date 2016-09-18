1b8d07578f37480720009a08b361d3e7b2bd234e
pascalewalters <pascalewalters@gmail.com>
2016-09-17 22:30:41 -0400

    Added pulse sensor stuff

---
+++ PulseSensorAmped_Arduino_1dot4/AllSerialHandling.ino
@@ -0,0 +1,58 @@
+
+//////////
+/////////  All Serial Handling Code, 
+/////////  It's Changeable with the 'serialVisual' variable
+/////////  Set it to 'true' or 'false' when it's declared at start of code.  
+/////////
+
+void serialOutput(){   // Decide How To Output Serial. 
+ if (serialVisual == true){  
+     arduinoSerialMonitorVisual('-', Signal);   // goes to function that makes Serial Monitor Visualizer
+ } else{
+      sendDataToSerial('S', Signal);     // goes to sendDataToSerial function
+ }        
+}
+
+
+//  Decides How To OutPut BPM and IBI Data
+void serialOutputWhenBeatHappens(){    
+ if (serialVisual == true){            //  Code to Make the Serial Monitor Visualizer Work
+    Serial.print("*** Heart-Beat Happened *** ");  //ASCII Art Madness
+    Serial.print("BPM: ");
+    Serial.print(BPM);
+    Serial.print("  ");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("BPM");
+    lcd.setCursor(0,1);
+    lcd.print(BPM);
+    delay(1000);
+
+ } else{
+        sendDataToSerial('B',BPM);   // send heart rate with a 'B' prefix
+        sendDataToSerial('Q',IBI);   // send time between beats with a 'Q' prefix
+ }   
+}
+
+
+
+//  Sends Data to Pulse Sensor Processing App, Native Mac App, or Third-party Serial Readers. 
+void sendDataToSerial(char symbol, int data ){
+    Serial.print(symbol);
+
+    Serial.println(data);                
+  }
+
+
+//  Code to Make the Serial Monitor Visualizer Work
+void arduinoSerialMonitorVisual(char symbol, int data ){    
+  const int sensorMin = 0;      // sensor minimum, discovered through experiment
+const int sensorMax = 1024;    // sensor maximum, discovered through experiment
+
+  int sensorReading = data;
+  // map the sensor range to a range of 12 options:
+  int range = map(sensorReading, sensorMin, sensorMax, 0, 11);
+ 
+}
+
+
