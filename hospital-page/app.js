41d173bc819d96be7ae4d0d3874a6168f099b592
seanlazaro <seanchristienlazaro@gmail.com>
2016-09-17 23:45:03 -0400

    Patient info no longer hard-coded

--- hospital-page/app.js
+++ hospital-page/app.js
@@ -13,15 +13,49 @@ $(document).ready(function() {
 			$('#patient-dropdown').append(element);
 		}
 	}
-
-	
 	
 
 	$('#instance-list').hide();
 	$('#patient-info').hide();
 
+
 	$('#patient-dropdown').change(function() {
+		var chosenName = $('#patient-dropdown').val();
+		var dates = localStorage.getItem(chosenName);
+
+		if(dates != null){
+			var datesArr = dates.split(";");
+			var dataArr = [];
+
+			for(var i = 0; i < datesArr.length; i++){
+				var key = chosenName + datesArr[i];
+				dataArr.push("");
+				dataArr[i] = localStorage.getItem(key);
+			}
+		}
+
+		//hard-coded to use the first element of dataArr for now
+		var valsArr = dataArr[0].split(";");
+		var startIndex = 0;
+		var val;
+		var idArr = [
+			"#patient-name", 
+			"#patient-age",
+			"#patient-gender",
+			"#patient-id",
+			"#patient-hr",
+			"#patient-bp",
+			"#patient-temp"];
+
+		for(var i = 0; i < idArr.length; i+= 1){
+			startIndex = valsArr[i].indexOf("=");
+			startIndex += 1;
+			var val = valsArr[i].substring(startIndex, valsArr[i].length);
+			$(idArr[i]).text(val);
+		}
+		
 		$('#instance-list').show();
+		
 	});
 
 	$('#instance-list td').click(function() {
