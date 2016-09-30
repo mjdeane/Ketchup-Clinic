1a311eb0dec0410ebec006abf81d3df496e918fa
Sally Moon <sallymoon10@gmail.com>
2016-09-29 21:27:30 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	hospital-page/index.html

--- patient-profile/app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-	$('.instance-list').css("display", "inline");
-
-	$('.instance-list td').click(function() {
-		$('.session-info').css("display", "inline");
-	});
-});
